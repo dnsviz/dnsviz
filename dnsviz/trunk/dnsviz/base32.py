748979bc55d616eef8f1f91462c65c6ac06bf590
casey@deccio.net <casey@deccio.net@7885c203-b7ea-840a-efb4-8f8d899814cf>
2014-08-25 18:05:48 +0000

    Add python copyright notice

--- dnsviz/trunk/dnsviz/base32.py
+++ dnsviz/trunk/dnsviz/base32.py
@@ -1,3 +1,12 @@
+#
+# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+# 2011, 2012, 2013 Python Software Foundation; All Rights Reserved.
+#
+# The contents of this module are derived the base64 module of python 2.7, with
+# the value of _b32tab modified to use the Base 32 Encoding with Extended Hex
+# Alphabet, as specified in RFC 4648.
+# 
+
 import struct
 
 _b32tab = { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
