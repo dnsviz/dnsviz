764932dab35f39158f6eeb9e47aa36947f8c09da
Casey Deccio <casey@deccio.net>
2014-10-30 14:13:38 -0400

    Modify directory structure

--- dnsviz/branches/old-trunk/dnsviz/util/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-
-import base32, dnsutil, dnssec, format, log, nsec
--- dnsviz/branches/retro/dnsviz/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'dnsviz.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
