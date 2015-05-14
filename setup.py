0a1c0c42f42336b3230c8ef7ca0f70b9861e5338
Casey Deccio <casey@deccio.net>
2015-05-14 09:23:07 -0400

    Move digviz to contrib

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='dnsviz',
         long_description=open('README.md', 'r').read(),
         license='LICENSE',
         packages=['dnsviz','dnsviz.viz','dnsviz.analysis'],
-        scripts=['bin/dnsget', 'bin/dnsviz', 'bin/dnsgrok', 'bin/digviz'],
+        scripts=['bin/dnsget', 'bin/dnsviz', 'bin/dnsgrok'],
         data_files=[
                 ('share/doc/dnsviz', ['README.md', 'LICENSE']),
                 ('share/doc/dnsviz', ['doc/dnsviz.html']),
