5a729f29d9e71895bc96f90871a0bf468440403f
Casey Deccio <casey@deccio.net>
2015-06-02 20:40:59 -0400

    Add initial recursive server analysis

--- dnsviz/analysis/__init__.py
+++ dnsviz/analysis/__init__.py
@@ -1,2 +1,2 @@
-from online import Analyst, OnlineDomainNameAnalysis, PrivateAnalyst, get_client_addresses, NetworkConnectivityException, resolver
+from online import Analyst, OnlineDomainNameAnalysis, PrivateAnalyst, PrivateRecursiveAnalyst, get_client_addresses, NetworkConnectivityException, resolver
 from offline import OfflineDomainNameAnalysis
