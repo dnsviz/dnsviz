fb0b13536493fe987ff9461aff9634708d2821d2
Casey Deccio <casey@deccio.net>
2015-09-09 13:38:49 -0400

    Modify macro replacement

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ def apply_install_prefix(filename):
 
     in_fh = open(filename, 'r')
     out_fh = open(filename_out, 'w')
-    out_fh.write(in_fh.read().replace('__DNSVIZ_INSTALL_PREFIX__', INSTALL_DATA))
+    s = in_fh.read()
+    s = s.replace('__DNSVIZ_INSTALL_PREFIX__', INSTALL_DATA)
+    out_fh.write(s)
     in_fh.close()
     out_fh.close()
 
