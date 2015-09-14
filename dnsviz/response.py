aea1d5ad149f2b524786297c335485a75060becd
Casey Deccio <casey@deccio.net>
2015-09-14 14:43:07 -0400

    exclude invalid nsec3 hashes
    When checking for NSEC3 coverings, in addition to checking for invalid
    owner names, also check for invalid hashes.

--- dnsviz/response.py
+++ dnsviz/response.py
@@ -1072,7 +1072,7 @@ class NSECSet(DNSResponseComponent):
         response that cover the given name.'''
 
         excluding_names = set()
-        for nsec_name in set(self.rrsets).difference(self.invalid_nsec3_owner):
+        for nsec_name in set(self.rrsets).difference(self.invalid_nsec3_owner.union(self.invalid_nsec3_hash)):
             if self._nsec_covers_name(name, nsec_name):
                 excluding_names.add(nsec_name)
         return excluding_names
@@ -1093,7 +1093,7 @@ class NSECSet(DNSResponseComponent):
         response that cover the given (digest) name.'''
 
         excluding_names = set()
-        for nsec_name in set(self.nsec3_params[(salt, alg, iterations)]).difference(self.invalid_nsec3_owner):
+        for nsec_name in set(self.nsec3_params[(salt, alg, iterations)]).difference(self.invalid_nsec3_owner.union(self.invalid_nsec3_hash)):
             if self._nsec_covers_name(name, nsec_name):
                 excluding_names.add(nsec_name)
         return excluding_names
