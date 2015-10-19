e586c5ef6954e8b54b213ed8f662ed032339fabf
Casey Deccio <casey@deccio.net>
2015-10-19 13:15:58 -0400

    Use initial options for PMTU errors

--- dnsviz/analysis/errors.py
+++ dnsviz/analysis/errors.py
@@ -1320,6 +1320,7 @@ class PMTUExceeded(ResponseError):
     description_template = '%(description)s'
     required_params = ['pmtu_lower_bound', 'pmtu_upper_bound']
     references = ['RFC 6891, Sec. 6.2.6']
+    use_effective_query_tag = False
 
     def __init__(self, **kwargs):
         super(PMTUExceeded, self).__init__(**kwargs)
