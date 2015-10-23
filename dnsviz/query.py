377fde986bec04fb18fe356c8255b30a52115929
Casey Deccio <casey@deccio.net>
2015-10-23 15:54:11 -0400

    Compare params properly
    Make sure the option type is in the string

--- dnsviz/query.py
+++ dnsviz/query.py
@@ -1106,7 +1106,7 @@ class MultiQuery(object):
         for o in query.edns_options:
             s = StringIO.StringIO()
             o.to_wire(s)
-            edns_options_str += s.getvalue()
+            edns_options_str += struct.pack('!H', o.otype) + s.getvalue()
         params = (query.flags, query.edns, query.edns_max_udp_payload, query.edns_flags, edns_options_str, query.tcp)
         if params in self.queries:
             self.queries[params] = self.queries[params].join(query, bailiwick_map, default_bailiwick)
