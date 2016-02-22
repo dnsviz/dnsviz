f2f8360b8530b54669cc525a194badbe62099b74
Casey Deccio <casey@deccio.net>
2016-02-22 09:23:39 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ else:
     RAPHAEL_FILES = []
 
 setup(name='dnsviz',
-        version='0.5.0',
+        version='0.5.1',
         author='Casey Deccio',
         author_email='casey@deccio.net',
         url='https://github.com/dnsviz/dnsviz/',
