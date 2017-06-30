59243458179790e22001cc56c15024e067ab8cdd
Casey Deccio <casey@deccio.net>
2017-06-30 16:00:36 -0600

    Update copyright years

--- dnsviz/commands/query.py
+++ dnsviz/commands/query.py
@@ -6,6 +6,8 @@
 #
 # Copyright 2015-2016 VeriSign, Inc.
 #
+# Copyright 2016-2017 Casey Deccio.
+#
 # DNSViz is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
