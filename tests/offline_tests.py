3024f1cc01c2b899c687a98f79862da13dd954d1
Casey Deccio <casey@deccio.net>
2019-01-11 22:02:58 -0700

    Define common names at top

--- tests/offline_tests.py
+++ tests/offline_tests.py
@@ -4,6 +4,12 @@ import os
 import subprocess
 import unittest
 
+DATA_DIR = os.path.dirname(__file__)
+EXAMPLE_AUTHORITATIVE = os.path.join(DATA_DIR, 'data', 'example-authoritative.json.gz')
+EXAMPLE_RECURSIVE = os.path.join(DATA_DIR, 'data', 'example-recursive.json.gz')
+ROOT_AUTHORITATIVE = os.path.join(DATA_DIR, 'data', 'root-authoritative.json.gz')
+ROOT_RECURSIVE = os.path.join(DATA_DIR, 'data', 'root-recursive.json.gz')
+
 class DNSGrokTestCase(unittest.TestCase):
     def setUp(self):
         self.devnull = io.open('/dev/null', 'wb')
@@ -12,25 +18,25 @@ class DNSGrokTestCase(unittest.TestCase):
         self.devnull.close()
 
     def test_grok_root_authoritative(self):
-        with gzip.open('%s/data/root-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'grok'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_grok_root_recursive(self):
-        with gzip.open('%s/data/root-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'grok'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_grok_example_authoritative(self):
-        with gzip.open('%s/data/example-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'grok'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_grok_example_recursive(self):
-        with gzip.open('%s/data/example-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'grok'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
@@ -43,25 +49,25 @@ class DNSPrintTestCase(unittest.TestCase):
         self.devnull.close()
 
     def test_print_root_authoritative(self):
-        with gzip.open('%s/data/root-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'print'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_print_root_recursive(self):
-        with gzip.open('%s/data/root-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'print'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_print_example_authoritative(self):
-        with gzip.open('%s/data/example-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'print'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_print_example_recursive(self):
-        with gzip.open('%s/data/example-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'print'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
@@ -74,25 +80,25 @@ class DNSGraphTestCase(unittest.TestCase):
         self.devnull.close()
 
     def test_graph_root_authoritative(self):
-        with gzip.open('%s/data/root-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'graph', '-Thtml'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_graph_root_recursive(self):
-        with gzip.open('%s/data/root-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(ROOT_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'graph', '-Thtml'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_graph_example_authoritative(self):
-        with gzip.open('%s/data/example-authoritative.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_AUTHORITATIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'graph', '-Thtml'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
 
     def test_graph_example_recursive(self):
-        with gzip.open('%s/data/example-recursive.json.gz' % os.path.dirname(__file__)) as fh:
+        with gzip.open(EXAMPLE_RECURSIVE) as fh:
             p = subprocess.Popen(['dnsviz', 'graph', '-Thtml'], stdin=subprocess.PIPE, stdout=self.devnull)
             p.communicate(input=fh.read())
             self.assertEqual(p.returncode, 0)
