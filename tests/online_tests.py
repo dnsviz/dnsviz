66c01b6a65c5cfdb49428cc24ba5a6dacb8c7fbc
Casey Deccio <casey@deccio.net>
2020-11-09 15:15:10 -0700

    Use relative path rather than no path

--- tests/online_tests.py
+++ tests/online_tests.py
@@ -10,16 +10,16 @@ class DNSProbeTestCase(unittest.TestCase):
         self.devnull.close()
 
     def test_authoritative_probe_root(self):
-        self.assertEqual(subprocess.call(['dnsviz', 'probe', '-d', '0', '-A', '.'], stdout=self.devnull), 0)
+        self.assertEqual(subprocess.call(['./bin/dnsviz', 'probe', '-d', '0', '-A', '.'], stdout=self.devnull), 0)
 
     def test_authoritative_probe_example_com(self):
-        self.assertEqual(subprocess.call(['dnsviz', 'probe', '-d', '0', '-A', 'example.com'], stdout=self.devnull), 0)
+        self.assertEqual(subprocess.call(['./bin/dnsviz', 'probe', '-d', '0', '-A', 'example.com'], stdout=self.devnull), 0)
 
     def test_recursive_probe_root(self):
-        self.assertEqual(subprocess.call(['dnsviz', 'probe', '-d', '0', '.'], stdout=self.devnull), 0)
+        self.assertEqual(subprocess.call(['./bin/dnsviz', 'probe', '-d', '0', '.'], stdout=self.devnull), 0)
 
     def test_recursive_probe_example_com(self):
-        self.assertEqual(subprocess.call(['dnsviz', 'probe', '-d', '0', 'example.com'], stdout=self.devnull), 0)
+        self.assertEqual(subprocess.call(['./bin/dnsviz', 'probe', '-d', '0', 'example.com'], stdout=self.devnull), 0)
 
 if __name__ == '__main__':
     unittest.main()
