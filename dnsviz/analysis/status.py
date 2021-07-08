8bb6d2f067a5a1aa63a62ae0ffd4b9cbb7f9d3af
Jan Včelák <jvcelak@ns1.com>
2021-07-08 16:55:39 +0200

    fix comparing record TTL and RRSIG expiration time

--- dnsviz/analysis/status.py
+++ dnsviz/analysis/status.py
@@ -279,7 +279,7 @@ class RRSIGStatus(object):
             if self.validation_status == RRSIG_STATUS_VALID:
                 self.validation_status = RRSIG_STATUS_EXPIRED
             self.errors.append(Errors.ExpirationInPast(expiration=fmt.timestamp_to_datetime(self.rrsig.expiration), reference_time=fmt.timestamp_to_datetime(self.reference_ts)))
-        elif self.reference_ts + min_ttl >= self.rrsig.expiration:
+        elif self.reference_ts + min_ttl > self.rrsig.expiration:
             self.errors.append(Errors.TTLBeyondExpiration(expiration=fmt.timestamp_to_datetime(self.rrsig.expiration), rrsig_ttl=min_ttl, reference_time=fmt.timestamp_to_datetime(self.reference_ts)))
         elif self.reference_ts + CLOCK_SKEW_WARNING >= self.rrsig.expiration:
             self.warnings.append(Errors.ExpirationWithinClockSkew(expiration=fmt.timestamp_to_datetime(self.rrsig.expiration), reference_time=fmt.timestamp_to_datetime(self.reference_ts)))
