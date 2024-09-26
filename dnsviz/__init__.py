a2c67620a44c8cd44e73c827fa2ae150416ab334
Casey Deccio <casey@deccio.net>
2024-09-26 16:13:37 -0600

    Add compatibility for python < 3.8

--- dnsviz/__init__.py
+++ dnsviz/__init__.py
@@ -1,3 +1,9 @@
-import importlib.metadata
-
-dist = importlib.metadata.distribution(__name__)
+try:
+    import importlib.metadata
+except ImportError:
+    # compatibility with python < 3.8
+    class Placeholder(object):
+        version = ''
+    dist = Placeholder()
+else:
+    dist = importlib.metadata.distribution(__name__)
