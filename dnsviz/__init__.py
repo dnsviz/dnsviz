90f40b7e07676120466fd95493065c3d8101ea56
Casey Deccio <casey@deccio.net>
2025-04-19 21:11:19 -0600

    Use GenericOption for parsing.
    This make DNSViz compatible with dnspython versions before and after
    dnspython 2.7.0.  Fixes #151 and #161.

--- dnsviz/__init__.py
+++ dnsviz/__init__.py
@@ -1,4 +1,12 @@
 try:
+    # compatibility with dnspython >= 2.7.0
+    from dns.edns import GenericOption, OptionType, register_type
+    for o in list(OptionType):
+      register_type(GenericOption, o)
+except ImportError:
+    pass
+
+try:
     import importlib.metadata
 except ImportError:
     # compatibility with python < 3.8
